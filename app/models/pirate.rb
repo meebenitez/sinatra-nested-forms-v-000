409ad93c7359e3ee8fa44169d1aa216a08e3595a
Mary Benitez <meebenitez@gmail.com>
2017-10-16 16:21:25 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,12 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+
+  PIRATES = []
+
+
+  def self.all
+    PIRATES
+  end
+
+  
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,14 @@
 class Ship
+  attr_accessor :name, :type, :booty
+
+  SHIPS = []
+
+  def self.all
+    SHIPS
+  end
+
+  def self.clear
+    SHIPS.clear
+  end
+
 end
