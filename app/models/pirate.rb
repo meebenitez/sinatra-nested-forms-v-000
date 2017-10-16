c49e941a6f30118c5cf5df008f4a372e296a67fa
Mary Benitez <meebenitez@gmail.com>
2017-10-16 20:40:50 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -3,10 +3,17 @@ class Pirate
 
   PIRATES = []
 
+  def initialize(params)
+    @name = params[:name]
+    @weight = params[:weight]
+    @height = params[:height]
+    PIRATES << self
+  end
+
 
   def self.all
     PIRATES
   end
 
-  
+
 end
