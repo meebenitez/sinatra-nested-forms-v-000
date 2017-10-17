2bc30e3d8614bbaa07a021670cf6ad37e328e4be
Mary Benitez <meebenitez@gmail.com>
2017-10-17 05:40:37 +0000

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -3,6 +3,13 @@ class Ship
 
   SHIPS = []
 
+  def initialize(params)
+    @name = params[:name]
+    @type = params[:type]
+    @booty = params[:booty]
+    SHIPS << self
+  end
+
   def self.all
     SHIPS
   end
