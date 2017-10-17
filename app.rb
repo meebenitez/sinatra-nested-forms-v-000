6f3cac24bfe9cb981c69f44af4981171773557b0
Mary Benitez <meebenitez@gmail.com>
2017-10-17 05:35:48 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -14,8 +14,8 @@ module FormsLab
     post '/pirates' do
       @pirate = Pirate.new(params[:pirate])
 
-      params[:pirate][:ships].each do |info|
-        Ship.new(info)
+      params[:pirate][:ships].each do |details|
+        Ship.new(details)
       end
 
       @ships = Ship.all
